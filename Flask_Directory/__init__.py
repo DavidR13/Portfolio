a0771bb12932e9f60ae0f013339f5435a28cfe54
David Rojas <70168138+DavidR13@users.noreply.github.com>
2022-01-01 21:17:39 -0500

    Fixed environment variables issue

--- Flask_Directory/__init__.py
+++ Flask_Directory/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+app.config["SECRET_KEY"] = os.environ['SECRET_KEY']
 
 from Flask_Directory import routes, auth
